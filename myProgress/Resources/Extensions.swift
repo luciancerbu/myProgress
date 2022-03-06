e283b7a7eff852abdea780ad23d334f1a449e8f6
Lucian Cerbu <luciancerbu87@gmail.com>
2022-03-06 23:06:00 +0200

    2.1Sign in UI
    Added UI elements for sign in

---
+++ myProgress/Resources/Extensions.swift
@@ -0,0 +1,35 @@
+//
+//  Extensions.swift
+//  myProgress
+//
+//  Created by Lucian Cerbu on 06.03.2022.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    var top: CGFloat {
+        frame.origin.y
+    }
+    
+    var bottom: CGFloat {
+        frame.origin.y+height
+    }
+    
+    var left: CGFloat {
+        frame.origin.x
+    }
+    
+    var right: CGFloat {
+        frame.origin.x+width
+    }
+    
+    var width: CGFloat {
+        frame.size.width
+    }
+    
+    var height: CGFloat {
+        frame.size.height
+    }
+}
---
+++ myProgress/Views/IGTextField.swift
@@ -0,0 +1,26 @@
+//
+//  IGTextField.swift
+//  myProgress
+//
+//  Created by Lucian Cerbu on 06.03.2022.
+//
+
+import UIKit
+
+class IGTextField: UITextField {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 50))
+        leftViewMode = .always
+        layer.cornerRadius = 8
+        layer.borderWidth = 1
+        layer.borderColor = UIColor.secondaryLabel.cgColor
+        backgroundColor = .secondarySystemBackground
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
