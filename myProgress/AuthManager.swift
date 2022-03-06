678f74244d7ccae6e983cb56ea642f584d35948f
Lucian Cerbu <luciancerbu87@gmail.com>
2022-03-06 17:02:45 +0200

    1.2Setting Up Firebase
    Configure Firebase console for login, storage, analytics.

--- myProgress/AuthManager.swift
+++ myProgress/AuthManager.swift
@@ -5,4 +5,13 @@
 //  Created by Lucian Cerbu on 06.03.2022.
 //
 
+import FirebaseAuth
 import Foundation
+
+final class AuthManager {
+    static let shared = AuthManager()
+    
+    private init() {}
+    
+    let auth = Auth.auth()
+}
